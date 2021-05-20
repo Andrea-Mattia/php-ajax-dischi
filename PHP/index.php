d67d36651c7b0507a0b9deaf7924ffaf36d16c9a
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-05-20 15:11:11 +0200

    add milestone 1 (PHP)

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,7 @@
+<?php
+include_once __DIR__ . '/partials/database.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +14,28 @@
 </head>
 <body>
     
+    <div class="df" id="app">
+        <header class="main-header">
+            <div class="container">
+                <img class="logo" src="./img/logo.png" alt="spotify-logo">
+            </div>
+        </header>
+        <main class="main-content">
+            <div class="card-container container df">
+                <?php foreach ($database as $album) { ?>
+                    <div class="card df">
+                        <img src="<?php echo $album['poster']; ?>" alt="<?php echo $album['title']; ?>">
+                        <div class="album-info">
+                            <h3><?php echo $album['title']; ?></h3>
+                            <p><?php echo $album['author']; ?></p>
+                            <h3><?php echo $album['year']; ?></h3>
+                            <p><?php echo $album['genre']; ?></p>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </main>
+    </div>
+
 </body>
 </html>
